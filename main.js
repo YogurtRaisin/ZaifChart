b35a094fc08c826ee05c70c0235491f4fd59405a
LAPTOP-GI1HS0Q4\kbysk <kbysknz@gmail.com>
2018-01-01 05:48:16 +0900

    Modify

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {app, Menu, Tray, BrowserWindow, nativeImage} = require('electron');
 const fs = require('fs');
 
 const setWin = require("./setWin.json");
 const path = require('path').join(__dirname, 'setWin.json');
 
 let mainWindow = null;
+let tray = null;
+
+const icon = __dirname + '/icon.png';
 
 app.on('window-all-closed', function () {
   if (process.platform != 'darwin')
@@ -16,6 +17,23 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
+    tray = new Tray(nativeImage.createFromPath(__dirname + "/icon.png"));
+
+    // タスクトレイに右クリックメニューを追加
+    var contextMenu = Menu.buildFromTemplate([
+        { label: "表示", click: function () { mainWindow.focus(); } },
+        { label: "終了", click: function () { mainWindow.close(); } }
+    ]);
+    tray.setContextMenu(contextMenu);
+
+    // タスクトレイのツールチップをアプリ名に
+    tray.setToolTip(app.getName());
+
+    // タスクトレイが左クリックされた場合、アプリのウィンドウをアクティブに
+    tray.on("clicked", function () {
+        mainWindow.focus();
+    });
+
 
   mainWindow = new BrowserWindow({
     width: 400,
@@ -26,8 +44,11 @@ app.on('ready', function () {
     minHeight: 60,
     resizable: true,
     frame: false,
-    transparent: true
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true
    });
+
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 
   mainWindow.setPosition(setWin["x"], setWin["y"]);
