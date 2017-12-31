abf1793df332e389cf8b052b60ecc3f6ce5fad52
LAPTOP-GI1HS0Q4\kbysk <kbysknz@gmail.com>
2018-01-01 05:53:54 +0900

    Modify

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const {app, Menu, Tray, BrowserWindow, nativeImage} = require('electron');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const fs = require('fs');
 
 const setWin = require("./setWin.json");
@@ -11,13 +13,47 @@ let tray = null;
 
 const icon = __dirname + '/icon.png';
 
-app.on('window-all-closed', function () {
-  if (process.platform != 'darwin')
+// GCされないようにグローバル宣言
+let mainWindow = null;
+var Menu = null;
+var Tray = null;
+var nativeImage = null;
+var tray = null;
+var contextMenu = null;
+var clickable = true;
+// 全てのウィンドウが閉じたら終了
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
     app.quit();
+  }
 });
 
 app.on('ready', function () {
-    tray = new Tray(nativeImage.createFromPath(__dirname + "/icon.png"));
+  mainWindow = new BrowserWindow({
+    width: 400,
+    height: 60,
+    maxWidth: 400,
+    maxHeight: 80,
+    minWidth: 150,
+    minHeight: 60,
+    resizable: true,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    skipTaskbar: true
+   });
+
+  mainWindow.setIgnoreMouseEvents(!clickable)
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+
+  mainWindow.setPosition(setWin["x"], setWin["y"]);
+  mainWindow.setSize(setWin["width"], setWin["height"]);
+
+  // タスクトレイに格納
+  Menu = electron.Menu;
+  Tray = electron.Tray;
+  nativeImage = electron.nativeImage;
+  tray = new Tray(nativeImage.createFromPath(__dirname + "/icon.png"));
 
     // タスクトレイに右クリックメニューを追加
     var contextMenu = Menu.buildFromTemplate([
@@ -35,24 +71,6 @@ app.on('ready', function () {
     });
 
 
-  mainWindow = new BrowserWindow({
-    width: 400,
-    height: 60,
-    maxWidth: 400,
-    maxHeight: 80,
-    minWidth: 150,
-    minHeight: 60,
-    resizable: true,
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    skipTaskbar: true
-   });
-
-  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-
-  mainWindow.setPosition(setWin["x"], setWin["y"]);
-  mainWindow.setSize(setWin["width"], setWin["height"]);
 
   mainWindow.on('close', function () {
     let item = JSON.stringify(mainWindow.getBounds());
